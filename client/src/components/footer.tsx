79e0c90e200df52386144d6301245b7764c31883
GWen <75674512+GWen124@users.noreply.github.com>
2025-11-19 10:44:15 +0800

    Update footer.tsx

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -59,7 +59,7 @@ function Footer() {
                             setDoubleClickTimes(doubleClickTimes + 1)
                         }
                     }}>
-                        © {new Date().getFullYear()} <a className='link-green' href="https://blog.gw124.top" target="_blank" rel="noopener noreferrer">BLOG.GW124.TOP</a> · Powered By <a className='link-green' href="https://github.com/openRin/Rin" target="_blank" rel="noopener noreferrer">Rin</a>
+                        © {new Date().getFullYear()} <a className='link-green' href="https://blog.gw124.com" target="_blank" rel="noopener noreferrer">BLOG.GW124.COM</a> · Powered By <a className='link-green' href="https://github.com/openRin/Rin" target="_blank" rel="noopener noreferrer">Rin</a>
                     </span>
                     {config.get<boolean>('rss') && <>
                         <Spliter />
